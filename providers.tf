f1d21b2e9e3b9a5c132b825bd44302155afa1c7c
James Bernardi <jamesbernardi@users.noreply.github.com>
2023-04-19 16:04:50 -0400

    test vars

--- providers.tf
+++ providers.tf
@@ -8,3 +8,10 @@ terraform {
 
   required_version = "~> 1.2"
 }
+
+provider "aws" {
+  region              = var.region
+  allowed_account_ids = [var.allowed_account_ids]
+  access_key          = var.aws_access_key
+  secret_key          = var.aws_secret_access_key
+}
--- variables.tf
+++ variables.tf
@@ -1,2 +1,6 @@
 variable "domain" {}
 variable "url" {}
+variable "region" {}
+variable "allowed_account_ids" {}
+variable "aws_secret_access_key" {}
+variable "aws_access_key" {}
